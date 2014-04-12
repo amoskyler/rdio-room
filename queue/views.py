4b2b90d387c4c53c6a88ad0ab484c1f1242d3bd0
amoskyler <amos.kyler@gmail.com>
2014-04-12 04:49:03 -0700

    added HttpResponse with second attribute

--- queue/views.py
+++ queue/views.py
@@ -3,11 +3,14 @@ import twilio.twiml
 from django.views.generic import View
 from django.http import HttpResponse
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
 
 class Queue(View):
 
+    @csrf_exempt
     def get(self, request):
         res = twilio.twiml.Response()
         res.message("Hello, Mobile Monkey")
         print str(res)
-        return HttpResponse("it worked")
+        return HttpResponse(twiml, content_type='text/xml')
---
+++ rdioroom/rdioroom/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from queue import views
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'rdioroom.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', 'djtwilio.views.sms'),
+    url(r'^admin/', include(admin.site.urls)),
+)
