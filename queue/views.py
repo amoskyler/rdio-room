91e84864be61cdfddaf4850e5ac517f2be26158f
amoskyler <amos.kyler@gmail.com>
2014-04-12 15:52:14 -0700

    completely broken views.py file

--- queue/views.py
+++ queue/views.py
@@ -7,21 +7,25 @@ from django.http import HttpResponse
 from rdio import Rdio
 # Create your views here.
 
+def searchRdio(query):
+    rdio = Rdio(("WF00svqcVrTXHv3eHSQO1w", "FYAjVfujchx4eTCFAOD8ag"))
+    results = rdio.call("search", {"query" :  query,
+                                                "types" : ["Track",],
+                                                "extras" : "isExplicit",
+                                                "count" : '1'})
+    return results
+
+
 class Queue(View):
 
     def get(self, request):
-        res = twilio.twiml.Response()
-        body = request.GET['Body']
-        results = searchRdio(body)
-        print results
-        twiml = '<Response><Message>message of ' +body+ ' was recieved</Message></Response>'
-        res.message("You're song "+results + "has been found")
-        return HttpResponse(twiml+ '/n' + results, content_type='text/xml')
+        #res = twilio.twiml.Response()
+        #body = request.GET['Body']
+        results = searchRdio("weezer el scorcho")
+        #print results
 
-        def searchRdio(query):
-            rdio = Rdio(("WF00svqcVrTXHv3eHSQO1w", "FYAjVfujchx4eTCFAOD8ag"))
-            results = rdio.call("search", {"query" :  query,
-                                                        "types" : ["Track",],
-                                                        "extras" : "isExplicit",
-                                                        "count" : 1})
-            return results[0]
+        #twiml = '<Response><Message>message of ' +body+ ' was recieved</Message></Response>'
+        #res.message("You're song "+results + "has been found")
+        return HttpResponse("adfa")
+
+
