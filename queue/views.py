844e0a44a5bf2cef05751a95eb4ed89e822afe3c
amoskyler <amos.kyler@gmail.com>
2014-04-12 04:50:54 -0700

    changed triml response

--- queue/views.py
+++ queue/views.py
@@ -4,13 +4,15 @@ from django.views.generic import View
 from django.http import HttpResponse
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
-
-
 class Queue(View):
 
     @csrf_exempt
     def get(self, request):
         res = twilio.twiml.Response()
         res.message("Hello, Mobile Monkey")
+
+        twiml = '<Response><Message>Hello from your Django app!</Message></Response>'
+
+
         print str(res)
         return HttpResponse(twiml, content_type='text/xml')
