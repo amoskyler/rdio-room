1877def271631fb5c9fb2da1ecf223e60822d4fb
amoskyler <amos.kyler@gmail.com>
2014-07-17 17:52:54 -0700

    check if logged in on front end

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,14 @@ $(document).ready(function(){
 
   var IndexView = Backbone.View.extend({
     render:function(){
-      this.$el.html("<a href = 'http://amos.ngrok.com'>LOGIN</a>")
-      return this;
+      if(_loggedIn === true){
+          this.$el.html("You are not logged in");
+          return this;
+      }
+      else if(_loggedIn === false){
+        this.$el.html("You are logged in");
+        return this;
+      };
     }
   });
   var AppRouter = Backbone.Router.extend({
